20fc6349f206cf5cc7a8154f051c21febb4d33a8
Ivens <ivensarroxellas@hotmail.com>
2022-06-07 17:15:46 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,12 @@
+const botaoL = document.getElementById('botaLogin');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+function handleSubmit() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456'){
+    alert("Olá, Tryber!")
+  } else {
+    alert("Email ou senha inválidos")
+  }
+}
+botaoL.addEventListener('click', handleSubmit);
