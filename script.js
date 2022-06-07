76ba73518f62371c93241f544d293b7b1437de58
Ivens <ivensarroxellas@hotmail.com>
2022-06-07 18:09:20 -0300

    arruma 6

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
+// eslint-disable-next-line no-unused-vars
 function handleSubmit() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
