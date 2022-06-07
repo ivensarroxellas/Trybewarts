2f44b749255ccb3bc1faf54b4061ad4afee7b69c
Ivens <ivensarroxellas@hotmail.com>
2022-06-07 18:22:56 -0300

    arruma 7,8 e 9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+const botaoL = document.getElementById('botaoLogin')
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-// eslint-disable-next-line no-unused-vars
 function handleSubmit() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
@@ -9,3 +9,4 @@ function handleSubmit() {
     alert('Email ou senha inválidos.');
   }
 }
+botaoL.addEventListener('click', handleSubmit);
